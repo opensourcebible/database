35e03a8403e3b66e9bc07ec10f4103a30517e8bb
Rafael Neri <rafepel@gmail.com>
2022-06-03 11:46:32 -0300

    Refactor

--- src/commands/all/index.ts
+++
@@ -1,9 +0,0 @@
-import { Command, Flags } from '@oclif/core';
-
-export default class Index extends Command {
-  static description = 'Generate All files Database';
-
-  async run() {
-    this.log(Index.description);
-  }
-}
--- src/commands/bible/mybible.ts
+++
@@ -1,9 +0,0 @@
-import { Command, Flags } from '@oclif/core';
-
-export default class Mybible extends Command {
-  static description = 'Generate files from MyBible';
-
-  async run() {
-    
-  }
-}
