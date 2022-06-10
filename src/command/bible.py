1b65def74b78bcedeb27fc100c31815fa6fcbbac
Rafael Neri <rafepel@gmail.com>
2022-06-10 02:21:08 -0300

    Refactor Bible

--- src/command/bible.py
+++ src/command/bible.py
@@ -1,10 +1,18 @@
-"""Rotinas relacionadas ao uso da Bíblia."""
+"""Build Bible."""
 
 import typer
+from src.helper import build, filesystem
+from src.data import langs
 
 app = typer.Typer()
 
 
 @app.callback(invoke_without_command=True)
 def main():
-    print("Bibles")
+    build.build_dir()
+
+    langs_available = langs.execute()
+    filesystem.create_json_file("/langs.json", langs_available)
+    for lang in langs_available:
+        code = lang["code"]
+        filesystem.directory(code)
