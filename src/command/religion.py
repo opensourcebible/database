92fed61bf01f6904c8d2753269b1ec6108af215f
Rafael Neri <rafepel@gmail.com>
2022-06-13 23:31:27 -0300

    Add Religion

---
+++ src/command/religion.py
@@ -0,0 +1,42 @@
+"""Load All Religions Available."""
+
+import typer
+import os
+import tomli
+
+from src.config import main as config
+from src.helper import filesystem
+
+app = typer.Typer()
+
+
+@app.callback(invoke_without_command=True)
+def main():
+    lang_dir = config.data_path + "/religions"
+    langs = []
+
+    try:
+        for lang_filename in os.listdir(lang_dir):
+            with open(os.path.join(lang_dir, lang_filename), "r") as lang_file:
+                lang = tomli.loads(lang_file.read()).get("lang")
+                langs.append(
+                    {
+                        "code": lang["code"],
+                        "name": lang["name"],
+                        "religions": {
+                            "url": config.base_url + "/" + lang["code"] + "/religions.json",
+                        },
+                    }
+                )
+
+                filesystem.directory(lang["code"])
+
+        # Sort
+        langs.sort(key=lambda obj: obj["code"])
+
+    except Exception as ex:
+        print(str(ex))
+
+    filesystem.create_json_file("/langs.json", langs)
+
+    print("Load All Languages Available [OK]")
