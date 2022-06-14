4ef223c60c181d5d29c613ac61bb04799f4a29c6
Rafael Neri <rafepel@gmail.com>
2022-06-13 23:33:25 -0300

    refactor

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
 import typer
-from src.command import bible, comment
+import importlib
+import os
+
+from src.config import main as config
 
 app = typer.Typer(add_completion=True, add_help_option=False)
 
-"""Carrega os comandos."""
-app.add_typer(bible.app, name="bible")
-app.add_typer(comment.app, name="comment")
+"""Load All Commands Available."""
+for command_file in os.listdir(config.command_path):
+    if command_file.endswith(".py"):
+        command_name = command_file.replace(".py", "")
+        command_module = importlib.import_module("src.command.%s" % command_name)
+        app.add_typer(command_module.app, name=command_name)
