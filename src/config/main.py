fceb7692881123338d4b3ec6908000c13159742d
Rafael Neri <rafepel@gmail.com>
2022-06-13 23:41:18 -0300

    refactor

--- src/config/main.py
+++ src/config/main.py
@@ -8,6 +8,9 @@ config_path = os.path.abspath(os.path.dirname(__file__))
 base_path = config_path + "/.."
 
 build_path = base_path + "/../build"
+
 data_path = base_path + "/data"
 
 public_path = base_path + "/../public"
+
+command_path = base_path + "/command"
